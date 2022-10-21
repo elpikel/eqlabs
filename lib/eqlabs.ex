f5a935e4607c8f98614aa62473842704b0769929
Arkadiusz Plichta <el.pikel@gmail.com>
2022-10-21 10:21:01 +0200

    init

---
+++ lib/eqlabs.ex
@@ -0,0 +1,18 @@
+defmodule Eqlabs do
+  @moduledoc """
+  Documentation for `Eqlabs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eqlabs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eqlabs_test.exs
@@ -0,0 +1,8 @@
+defmodule EqlabsTest do
+  use ExUnit.Case
+  doctest Eqlabs
+
+  test "greets the world" do
+    assert Eqlabs.hello() == :world
+  end
+end
